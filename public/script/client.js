11042a5483440a5bd71fd602fa94bbd6b5a66d30
Ronel <shujihanma028@gmail.com>
2022-06-19 19:16:21 -0700

    add function to display data to UI

--- public/script/client.js
+++ public/script/client.js
@@ -22,13 +22,15 @@ let form = document.querySelector("#form");
 let title = document.querySelector("#input-title");
 let description = document.querySelector("#input-description");
 let content = document.querySelector("#input-content");
+let articleContainer = document.querySelector("#article-container");
 
 form.addEventListener("submit", (e)=>{
    e.preventDefault();
 
    insertData(title, description, content);
+   displayData();
 })
-// use axios to fetch data
+// insert function
 let insertData = async(inputTitle, inputDescription, inputContent)=>{
    let insert = await axios({
       method: 'post',
@@ -41,3 +43,38 @@ let insertData = async(inputTitle, inputDescription, inputContent)=>{
     });
    console.log(insert);
 }
+
+// display function
+let displayData = async()=>{
+   let display = await axios({
+      method: 'get',
+      url: '/displayArticle'
+   })
+
+   console.log(display)
+   articleContainer.innerHTML = displayToUi(display.data);
+}
+
+
+let displayToUi = (article)=>{
+    let articleContent = "";
+    article.forEach(art => {
+        articleContent += `
+         <li>
+            <div class="card" style="width: 18rem;">
+                  <div class="card-body">
+                     <h5 class="card-title">${art.article_title}</h5>
+                     <h6 class="card-subtitle mb-2 text-muted">${art.article_date}</h6>
+                     <p class="card-text">${art.article_subtitle}</p>
+                     <a href="#" class="btn btn-info text-light">Read More</a>
+                     <a href="#" class="btn btn-warning text-light">Edit</a>
+                     <a href="#" class="btn btn-danger text-light">Delete</a>
+                  </div>
+               </div>
+         </li>
+        `
+    });
+    return articleContent;
+}
+
+displayData();
